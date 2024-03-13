95954ad269981b8bf39b26cc01eb8e3d70da6915
4imaN <like93860@gmail.com>
2024-03-13 21:38:55 +0300

    changes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,12 @@
 
 import logo from "../assets/M_final.png";
 import { HashLink as Link } from "react-router-hash-link";
-<div className='flex justify-center items-center text-white px-5 md:px-14 lg:px-20 '>
+import { useState } from "react";
+
+const Nav = () => {
+    const [chatArrow, setChatArrow] = useState(false);
+    return (
+        <div className='flex justify-center items-center text-white px-5 md:px-14 lg:px-20 '>
             <div className='max-w-[1400px] w-full flex items-center justify-between'>
                 <div className='flex w-[50px]'>
                     <img src={logo} alt='mana' className='object-cover' />
@@ -13,8 +18,8 @@ import { HashLink as Link } from "react-router-hash-link";
                     <Link to='#services' smooth className="hover:scale-105 hover:text-blue-500">
                         Services
                     </Link>
-                    <Link to='#clients' smooth className="hover:scale-105 hover:text-blue-500">
-                        Our products
+                    <Link to='#products' smooth className="hover:scale-105 hover:text-blue-500">
+                        Our Products
                     </Link>
                     <Link to='#Contacts' smooth className="hover:scale-105 hover:text-blue-500">
                         Contacts
@@ -22,14 +27,14 @@ import { HashLink as Link } from "react-router-hash-link";
                 </div>
                 <div
                     className='flex items-center text-xl text-white gap-2 '
-                    
+
                 >
                     
                 
                 </div>
             </div>
         </div>
-    
-
+    );
+};
 
 export default Nav;
